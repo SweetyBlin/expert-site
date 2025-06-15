5ce69ea5dacb6f06a74fc7e6ca705f640d90860c
SweetyBlin <furletovden5@gmail.com>
2025-06-15 23:15:38 +0300

    Another update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 const btn = document.querySelector(".burger");
 const menu = document.querySelector(".header__bottom");
+let isScrollLocked = false;
 
 
 btn.addEventListener("click", () => {
-	btn.classList.toggle("animate");
-	menu.classList.toggle("animate");
+  btn.classList.toggle("animate");
+  menu.classList.toggle("animate");
+  if (isScrollLocked) {
+    document.body.style.overflow = "auto";
+    document.body.style.paddingRight = "0";
+  } else {
+    const scrollbarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
+    document.body.style.overflow = "hidden";
+    document.body.style.paddingRight = `${scrollbarWidth}px`;
+  }
+  isScrollLocked = !isScrollLocked;
 });
